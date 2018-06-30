cca0232c70fef065b9c5cb172c10f593bea40bcb
Justin Marion <justincmarion@gmail.com>
2018-06-29 21:22:24 -0500

    Change MainWindow to inherit from MetroWindow

--- src/OpenBudget.Presentation.Windows/Views/MainWindow.xaml.cs
+++ src/OpenBudget.Presentation.Windows/Views/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿namespace OpenBudget.Presentation.Windows.Views
 {
+    using MahApps.Metro.Controls;
+    using MahApps.Metro.Controls.Dialogs;
     using System.Windows;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         /// <summary>
         /// Initialises a new instance of the <see cref="MainWindow"/> class.
