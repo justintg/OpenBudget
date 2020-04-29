6a29ae0e5a2ced369c8a3e164dda6bece633d5a2
Justin Marion <justincmarion@gmail.com>
2020-04-28 21:53:35 -0500

    Rename EventSavingCallback to EventSaveInfo

---
+++ src/OpenBudget.Model/Infrastructure/UnitOfWork/EventSaveInfo.cs
@@ -0,0 +1,18 @@
+﻿using OpenBudget.Model.Events;
+using OpenBudget.Model.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenBudget.Model.Infrastructure.UnitOfWork
+{
+    public delegate void NotifyEventSavedHandler(ModelEvent evt);
+
+    public class EventSaveInfo
+    {
+        public bool NeedsAttach { get; set; }
+        public EntityBase Entity { get; set; }
+        public ModelEvent Event { get; set; }
+        public NotifyEventSavedHandler EventSavedCallback { get; set; }
+    }
+}
