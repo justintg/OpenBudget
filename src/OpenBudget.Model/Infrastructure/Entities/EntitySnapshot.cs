1fab76b2a0d7b39e180766ebe2c738142c3e3d1a
Justin Marion <justincmarion@gmail.com>
2020-05-12 17:03:34 -0500

    Make sure that events are not replayed on the source of the event by checking the LastEventID, fix storing of LastEventID and Vector on in memory snapshots

--- src/OpenBudget.Model/Infrastructure/Entities/EntitySnapshot.cs
+++ src/OpenBudget.Model/Infrastructure/Entities/EntitySnapshot.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace OpenBudget.Model.Infrastructure.Entities
 {
     public abstract class EntitySnapshot
     {
-        public string LastEventID { get; set; }
-        public VectorClock LastEventVector { get; set; }
         public string EntityID { get; set; }
         public EntityReference Parent { get; set; }
         public bool IsDeleted { get; set; }
+
+        public string LastEventID { get; set; }
+        public VectorClock LastEventVector { get; set; }
     }
 }
