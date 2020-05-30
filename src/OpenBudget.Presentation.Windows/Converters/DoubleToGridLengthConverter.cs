9421935ee1275cc2275c3c12db8a483b2fb51faf
Justin Marion <justincmarion@gmail.com>
2020-05-30 11:18:30 -0500

    Add add master category button

---
+++ src/OpenBudget.Presentation.Windows/Converters/DoubleToGridLengthConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace OpenBudget.Presentation.Windows.Converters
+{
+    public class DoubleToGridLengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double dblValue && targetType == typeof(GridLength))
+            {
+                return new GridLength(dblValue);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
