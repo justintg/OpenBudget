0d3f0fde02ee176ed3222f3de7fac42ca9c88862
Justin Marion <justincmarion@gmail.com>
2020-12-27 16:55:55 -0600

    add dynamic coloring to BudgetEditorCategoryMonth

---
+++ src/OpenBudget.Presentation.Windows/Converters/DecimalToBrushConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace OpenBudget.Presentation.Windows.Converters
+{
+    public class DecimalToBrushConverter : IValueConverter
+    {
+        public Brush PositiveBrush { get; set; } = Brushes.Black;
+        public Brush NegativeBrush { get; set; } = Brushes.Black;
+        public Brush ZeroBrush { get; set; } = Brushes.Black;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is decimal decimalValue)
+            {
+                if(decimalValue > 0)
+                {
+                    return PositiveBrush;
+                }
+                else if(decimalValue < 0)
+                {
+                    return NegativeBrush;
+                }
+                else
+                {
+                    return ZeroBrush;
+                }
+            }
+
+            throw new InvalidOperationException("This converter only supports decimal values");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
