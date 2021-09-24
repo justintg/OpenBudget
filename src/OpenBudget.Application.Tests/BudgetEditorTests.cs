774ce296d80b91bedf213440a55e5b1ff72ca9a3
Justin Marion <justincmarion@gmail.com>
2021-09-24 17:04:38 -0500

    Add Watermarks to Transaction Cells, Add Padding to Cells, Add Thumb to column headers

--- src/OpenBudget.Application.Tests/BudgetEditorTests.cs
+++ src/OpenBudget.Application.Tests/BudgetEditorTests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using OpenBudget.Application.ViewModels.BudgetEditor;
+using OpenBudget.Tests.Shared;
 using System;
 
 namespace OpenBudget.Application.Tests
@@ -6,9 +8,31 @@ namespace OpenBudget.Application.Tests
     [TestFixture]
     public class BudgetEditorTests
     {
+        public BudgetEditorTests()
+        {
+
+        }
+
+        public TestBudget TestBudget { get; private set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            TestBudget = BudgetSetup.CreateBudget(BudgetBackends.SQLite);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            TestBudget?.BudgetModel?.Dispose();
+            TestBudget = null;
+        }
+
         [Test]
         public void TestBudgetEditor()
         {
+            var budgetEditor = new BudgetEditorViewModel(TestBudget.BudgetModel);
+            budgetEditor.MakeNumberOfMonthsVisible(5);
 
         }
     }
