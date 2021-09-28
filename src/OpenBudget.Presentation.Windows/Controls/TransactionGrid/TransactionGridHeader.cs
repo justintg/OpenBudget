1131db62ba298bd3b369a42134c08b3e677e3173
Justin Marion <justincmarion@gmail.com>
2021-09-28 17:18:26 -0500

    Add a ResizeThumb to TransactionGridHeader

--- src/OpenBudget.Presentation.Windows/Controls/TransactionGrid/TransactionGridHeader.cs
+++ src/OpenBudget.Presentation.Windows/Controls/TransactionGrid/TransactionGridHeader.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,6 +21,27 @@ namespace OpenBudget.Presentation.Windows.Controls.TransactionGrid
         static TransactionGridHeader()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(TransactionGridHeader), new FrameworkPropertyMetadata(typeof(TransactionGridHeader)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var resizeThumb = this.GetTemplateChild("PART_ResizeThumb") as Thumb;
+            if (resizeThumb != null)
+            {
+                resizeThumb.DragStarted += ResizeStarted;
+                resizeThumb.DragDelta += ResizeDelta;
+            }
+        }
+
+        private void ResizeDelta(object sender, DragDeltaEventArgs e)
+        {
+            this.Width += e.HorizontalChange;
+        }
+
+        private void ResizeStarted(object sender, DragStartedEventArgs e)
+        {
+            var resizeStartWidth = this.ActualWidth;
         }
     }
 }
